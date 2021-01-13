f4df4e351ef85acbd2e473f9096b82a946bf9a33
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-01-13 15:29:26 +0500

    Chap1-2-3 complete

---
+++ chap1-2-3/index.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById('alert-btn')
+
+var firstName = 'Haseeb';
+
+var lastName = 'Ahmed';
+
+var age = 20;
+
+btn.addEventListener('click', () => {
+    alert("Hello "+firstName+" "+lastName+" you are "+age+" years old.")
+})
