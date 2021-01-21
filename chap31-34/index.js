8ef5a5bac5785b50e203e2f80efd0706ae6ed3c4
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-01-21 19:33:53 +0500

    Chap 31-34 completed

--- chap31-34/index.js
+++ chap31-34/index.js
@@ -1,20 +1,42 @@
 var start = document.getElementById('start-btn')
 
-// var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 start.addEventListener('click' , () => {
 
-    var name = prompt('Ener your name')
-    // var date = prompt('Enter your date of birth')
-    // var month = prompt('Enter your month of birth')
-    // var year = prompt('Enter your year of birth')
 
+    var fullName = prompt('Enter your full name')
+    var dob = prompt('Enter your date of birth')
+    var month = prompt("Enter your month of birth")
+    var mob = months.indexOf(month)
+    var yob = prompt('Enter your year of birth')
+
+    var d1 = new Date(yob , mob, dob)
+
+    var d2 = new Date()
+    var dateNow = d2.getDate();
+    var monthNow = months[d2.getMonth()]
+    var yearNow = d2.getFullYear()
+    var hourNow = d2.getHours()
+
+    if(hourNow > 0 && hourNow < 12){
+        alert('Good Morning '+fullName)
+    }else if(hourNow > 12 && hourNow < 4 ){
+        alert('Good Afternoon '+fullName)
+    }else{
+        alert('Good Evening '+fullName)
+    }
+
+    alert('Your date of birth is '+d1)
+
+    alert('You are '+(yearNow - yob)+' years, '+(mob > d2.getMonth() ? mob - d2.getMonth() : d2.getMonth() - mob)+' months, '+(dateNow > dob ? dateNow - dob : dob - dateNow)+' days old')
+
+    alert('Your age in days is '+(yearNow - yob)*365)
+
+    if(yearNow-yearNow===0 && dob-dateNow===0 && month - d2.getMonth()===0){
+        alert('Happy Birthday '+fullName)
+    }else{
+        alert('Time until next birthday? '+(mob-d2.getMonth())+' months, '+(31 - dateNow)+' days'+(d1.getHours())+' hours')
+    }
     
-    var d = new Date()
-    var dateNow = d.getDate()
-    var monthNow =d.getMonth()
-    var yearNow = d.getFullYear()
-    var timeNow = d.getHours()
-
-    alert(`Hello ${name}. You are  `)
 })
